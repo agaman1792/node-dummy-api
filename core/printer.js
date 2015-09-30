e952570c0d10c5ad62b7b212d4f970ef80876a0f
tzoky07 <alex.gaman@yahoo.com>
2015-10-01 00:45:37 +0300

    Updated "visuals"

--- core/printer.js
+++ core/printer.js
@@ -16,40 +16,50 @@ function print(what) {
   console.log(colorMixin(color)(message));
 }
 
+var colors = {
+  header: "cyan",
+  body: "blue"
+};
+
 function osDetailsPrinter() {
   var os = require("os");
   var uptime = os.uptime();
   uptime = moment(uptime).format("HH:mm:ss");
 
-  print({color: "yellow", message: "Listing operating system details below"});
-  print({color: "yellow", message: `Hostname: ${os.hostname()}`});
-  print({color: "yellow", message: `Type:     ${os.type()}`});
-  print({color: "yellow", message: `Arch:     ${os.arch()}`});
-  print({color: "yellow", message: `Release:  ${os.release()}`});
-  print({color: "yellow", message: `Uptime:   ${uptime}`});
+  print({color: colors.header, message: "*** OS details ***"});
+  print({color: colors.body, message: `Hostname: ${os.hostname()}`});
+  print({color: colors.body, message: `Type:     ${os.type()}`});
+  print({color: colors.body, message: `Arch:     ${os.arch()}`});
+  print({color: colors.body, message: `Release:  ${os.release()}`});
   print();
 }
 
 function serverConfigurationPrinter() {
   var cfg = config.get();
-  var connectionAddress = (cfg.server.security.enabled) ? "https://localhost:" : "http://localhost:";
-  connectionAddress += cfg.server.general.ports[0];
-  connectionAddress += cfg.server.general.prefix;
+  var baseUrl = (cfg.server.security.enabled) ? "https://localhost:" : "http://localhost:";
+  baseUrl += cfg.server.general.ports[0];
+  baseUrl += cfg.server.general.prefix;
 
-  print({color: "yellow", message: `Using the ${cfg.strategyUsed}`});
-  print({color: "yellow", message: `Api prefix:   ${cfg.server.general.prefix}`});
-  print({color: "yellow", message: `Port:         ${cfg.server.general.ports[0]}`});
-  print({color: "yellow", message: `Secure:       ${cfg.server.security.enabled}`});
-  print({color: "yellow", message: `Auth enabled: ${cfg.server.authorization.enabled}`});
-  print({color: "yellow", message: `Url:          ${connectionAddress}`});
+  print({color: colors.header, message: `*** Configuration details ***`});
+  print({color: colors.body, message: `Using the ${cfg.strategyUsed}`});
+  print({color: colors.body, message: `Api prefix:   ${cfg.server.general.prefix}`});
+  print({color: colors.body, message: `Port:         ${cfg.server.general.ports[0]}`});
+  print({color: colors.body, message: `Secure:       ${cfg.server.security.enabled}`});
+  print({color: colors.body, message: `Auth enabled: ${cfg.server.authorization.enabled}`});
+  print({color: colors.body, message: `Url:          ${baseUrl}`});
   print();
 }
 
 function serverEndpointDetailsPrinter() {
-  print({color: "yellow", message: "Listing server endpoint details below"});
   var cfg = config.get();
+  var baseUrl = (cfg.server.security.enabled) ? "https://localhost:" : "http://localhost:";
+  baseUrl += cfg.server.general.ports[0];
+  baseUrl += cfg.server.general.prefix;
+
+  print({color: colors.header, message: "*** Api endpoints ***"});
   cfg.api.routes.forEach((route) => {
-    print({color: "yellow", message: `Route ${route.path} (get, post, put, delete)... [OK]`});
+    print({color: colors.body, message: `Route ${route.path} (get, post, put, delete)... [OK]`});
+    print({color: colors.body, message: ` * Base endpoint: ${baseUrl + route.path}`});
   });
   print();
 }
@@ -59,13 +69,15 @@ function creditsNStuffPrinter() {
   var pck = require("../package.json");
   var securedText = (cfg.server.security.enabled) ? "(secured)" : "";
 
-  print({color: "yellow", message: "If you have any ideas about how to make this utility better, feel free to: "});
-  print({color: "yellow", message: " * Send me an e-mail: alex.gaman@yahoo.com"});
-  print({color: "yellow", message: " * Grab the code, materialize your idea and send a pull request: https://github.com/tzoky07/dummy-api"});
-  print({color: "yellow", message: " * Write it on a piece of paper and ask a pidgeon to bring it to me"});
-  print({color: "gray", message: `Listening on port ${cfg.server.general.ports[0]} ${securedText}`});
-  print({color: "gray", message: `Press CTRL + C to kill the server`});
-  print({color: "gray", message: `App version: ${pck.version}`});
+  print({color: colors.header, message: "*** Other stuff ***"});
+  print({color: colors.body, message: "If you have any ideas about how to make this utility better, feel free to: "});
+  print({color: colors.body, message: " * Send me an e-mail: alex.gaman@yahoo.com"});
+  print({color: colors.body, message: " * Grab the code, materialize your idea and send a pull request: https://github.com/tzoky07/dummy-api"});
+  print({color: colors.body, message: `If you see something annoying, this is the best place to say it: ${pck.bugs.url}`});
+  print();
+  print({color: colors.body, message: `Listening on port ${cfg.server.general.ports[0]} ${securedText}`});
+  print({color: colors.body, message: `App version: ${pck.version}`});
+  print({color: colors.body, message: `Press CTRL + C to kill the server`});
 }
 
 module.exports = (what) => {
